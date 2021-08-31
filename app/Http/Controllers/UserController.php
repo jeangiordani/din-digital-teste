a7c3a961504d0e40b346ee63fe6c6228451bfba1
Jean <jeangiordani290@gmail.com>
2021-08-31 18:45:09 -0300

    adicionada a criacao de usuarios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
     //
+    protected $model;
+
+    public function __construct(User $user)
+    {
+        $this->model = $user;
+    }
+
+    public function create(Request $request)
+    {
+
+
+        $validator = Validator::make($request->all(), [
+            "name" => "required|max:255|string",
+            "email" => "required|unique:users,email|max:255",
+            "password" => "required|min:6"
+        ]);
+
+        if ($validator->fails()) {
+            $validator = $validator->errors()->messages();
+            return response()->json(['errors' => $validator], 422);
+        } else {
+            $data = [
+                "name" => $request->get('name'),
+                "password" => bcrypt($request->get('password')),
+                "email" => $request->get('email')
+            ];
+        }
+
+        $user = $this->model->create($data);
+        return response()->json($user, 201);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,6 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/user', [UserController::class, 'create'])->name('user.create');
 
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
 Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
